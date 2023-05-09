143baf682f5f1aa572c85773e8704a9e586cec42
nvnnaidenov <nvnnaidenov@gmail.com>
2023-05-09 21:14:15 +0300

    add

---
+++ Chapter 001/001.PerimeterAndAreaRectangle/Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double a = 2.3;
+	double b = 3.7;
+
+	double p = 2 * (a * b);
+	double s = a * b;
+
+	cout << "p = " << p << endl;
+	cout << "s = " << s << endl;
+
+	return 0;
+}
