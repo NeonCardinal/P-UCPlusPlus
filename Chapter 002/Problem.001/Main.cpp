47145a12dc08ae3e5dddc8025a4172417b554855
nvnnaidenov <nvnnaidenov@gmail.com>
2023-05-10 20:27:43 +0300

    add

---
+++ Chapter 002/Problem.001/Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	double x;
+
+	cout << "x = ";
+	cin >> x;
+
+	int y = floor(x);
+
+	cout << x << endl;
+
+	return 0;
+}
