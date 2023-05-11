f06d4f027e0e4a892bdc297caaa68167008848c5
nvnnaidenov <nvnnaidenov@gmail.com>
2023-05-11 23:24:09 +0300

    add

---
+++ Chapter 003/Problem.009/Main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+
+	cout << "n = ";
+	cin >> n;
+	if (!cin)
+	{
+		cout << "Error, bad input!" << endl;
+		return 1;
+	}
+	if (n < 0 || n > 9)
+	{
+		cout << "Bad input!" << endl;
+		return 1;
+	}
+
+	if (n == 0)
+	{
+		cout << "zero" << endl;
+	}
+	else if (n == 1)
+	{
+		cout << "one" << endl;
+	}
+	else if (n == 2)
+	{
+		cout << "two" << endl;
+	}
+	else if (n == 3)
+	{
+		cout << "three" << endl;
+	}
+	else if (n == 4)
+	{
+		cout << "four" << endl;
+	}
+	else if (n == 5)
+	{
+		cout << "five" << endl;
+	}
+	else if (n == 6)
+	{
+		cout << "six" << endl;
+	}
+	else if (n == 7)
+	{
+		cout << "seven" << endl;
+	}
+	else if (n == 8)
+	{
+		cout << "eight" << endl;
+	}
+	else if (n == 9)
+	{
+		cout << "nine" << endl;
+	}
+
+	return 0;
+}
